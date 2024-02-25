c25718a0d4ca48b016e27f2efa5f8cdf9497bdc1
Md. Afzal Hassan Ehsani <iemafzalhassan@gmail.com>
2024-02-25 05:46:55 +0100

    Added some lines of code

--- motivator.py
+++ motivator.py
@@ -1,6 +1,21 @@
 import requests
-url = "https://quotes.rest/qod?language=en"
 
-res = requests.get(url=url)
-data = res.json()
-print(data['contents']['quotes'][0]['quote'])
+def get_quote_of_the_day(category):
+    url = "https://quotes.rest/qod?category={}".format(category)
+    res = requests.get(url=url)
+    data = res.json()
+    status = res.status_code
+    match status:
+        case 200:
+            quote = data['contents']['quotes'][0]['quote']
+        case 404:
+            quote = data["errror!"]["message"]
+        case _:
+            quote = "An error occurred"
+    return quote
+
+    quote = data['contents']['quotes'][0]['quote']
+
+quotw = get_quote_of_the_day(category="inspire")
+
+
